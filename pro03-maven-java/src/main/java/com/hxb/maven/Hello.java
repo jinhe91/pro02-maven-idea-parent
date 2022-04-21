b2bed65b0c04d544a6e7ed9c1a42705b5ab2f80c
jinhe <jinhe@lyzd.com>
2022-04-21 21:35:54 +0800

    第三次提交GIT

--- pro03-maven-java/src/main/java/com/hxb/maven/Hello.java
+++ pro03-maven-java/src/main/java/com/hxb/maven/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public void showMessage(){
         System.out.println("hello maven! I am in idea now!");
         System.out.println("hello git!");
+        System.out.println("hello git3!");
     }
 }
